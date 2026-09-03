b3dfc79ab09f8eec08699e9b7fec221bb9601e34
Murilo <murilopizolito@gmail.com>
2026-09-02 21:14:17 -0300

    feat: implementa tratamento global das exceções

---
+++ src/main/java/br/com/murilo/planeja/infra/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package br.com.murilo.planeja.infra.handlers;
+
+import br.com.murilo.planeja.common.exceptions.ValidationException;
+import br.com.murilo.planeja.common.validation.CampoInvalido;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<?> handleValidationException(ValidationException e) {
+        var status = HttpStatus.UNPROCESSABLE_CONTENT;
+        var body = Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", status.value(),
+                "error", e.getMessage(),
+                "camposInvalidos", e.getCampoInvalidos()
+        );
+
+        return ResponseEntity.status(status).body(body);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var camposInvalidos = e.getFieldErrors().stream()
+                .map(fieldError -> new CampoInvalido(fieldError.getField(), fieldError.getDefaultMessage()))
+                .toList();
+        var status = HttpStatus.UNPROCESSABLE_CONTENT;
+        var body = Map.of(
+                "timestamp", LocalDateTime.now(),
+                "status", status.value(),
+                "error", e.getMessage(),
+                "camposInvalidos", camposInvalidos
+        );
+
+        return ResponseEntity.status(status).body(body);
+    }
+
+
+}
