7d19145aaf58fd5265a40eeb012baab6a59ee596
Murilo <murilopizolito@gmail.com>
2026-09-06 15:46:54 -0300

    feat: implementa template da aplicação

--- planeja-app/src/app/app.ts
+++ planeja-app/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Template } from './template/template'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Template],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
