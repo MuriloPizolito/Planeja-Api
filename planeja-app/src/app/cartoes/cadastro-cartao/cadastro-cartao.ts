3b10ea86e4a2a5e8c657b5fcc7e1652ed3c2fe8f
Murilo <murilopizolito@gmail.com>
2026-09-11 17:59:56 -0300

    feat:  adiciona service para conexão com a API

--- planeja-app/src/app/cartoes/cadastro-cartao/cadastro-cartao.ts
+++ planeja-app/src/app/cartoes/cadastro-cartao/cadastro-cartao.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CartaoService } from '../cartao-service';
+import { DadosCartaoForm, DetalhesCartao } from '../dados-cartao';
 
 interface CadastroCartaoForm{
   nome: FormControl<string>;
@@ -14,6 +16,7 @@ interface CadastroCartaoForm{
 })
 export class CadastroCartao implements  OnInit {
   form!: FormGroup<CadastroCartaoForm>;
+  service = inject(CartaoService);
 
   ngOnInit(): void {
     this.form = new FormGroup<CadastroCartaoForm>({
@@ -24,6 +27,15 @@ export class CadastroCartao implements  OnInit {
 
   handleSubmit() {
     console.log(this.form.value)
+    const dadosCartao = this.form.value as DadosCartaoForm;
+    this.service.criar(dadosCartao)
+    .subscribe({
+      next: (response: DetalhesCartao) => {
+        console.log('recebendo a resposta do servidor: ', response);
+      },
+      error: (error) => console.log('ocorreu um erro: ', error)
+    });
+    
   }
 
 }
---
+++ planeja-app/src/app/cartoes/cartao-service.ts
@@ -0,0 +1,17 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {DadosCartaoForm, DetalhesCartao} from './dados-cartao'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartaoService {
+  http: HttpClient = inject(HttpClient);
+
+  criar(dados: DadosCartaoForm) : Observable<DetalhesCartao> {
+    const url = 'http://localhost:8080/cartoes'
+    return this.http.post<DetalhesCartao>(url, dados);
+  }
+
+}
